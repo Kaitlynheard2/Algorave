c1cee9df99555575f71e1d9b35047eadbb962e67
MyMac <mymac@MacBook-Pro-4.lan>
2021-01-13 16:20:37 -0800

    Final Algorave Website Fall 2020

---
+++ script.js
@@ -0,0 +1,251 @@
+y = 50; //added by Melody; tested on .type-effect2
+
+new TypeIt(".type-effect1", {
+  speed: 75,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".console-effect1", {
+  speed: 5,
+  startDelay: 3000,
+  lifeLike: false,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect2", {
+  speed: y, //added by Melody - tried using a variable to define the speed
+  startDelay: 3500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".console-effect2", {
+  speed: 5,
+  startDelay: 8500,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect3", {
+  speed: 75,
+  startDelay: 9500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect4", {
+  speed: 75,
+  startDelay: 11500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect5", {
+  speed: 75,
+  startDelay: 14500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect6", {
+  speed: 75,
+  startDelay: 16500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect7", {
+  speed: 75,
+  startDelay: 18500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect8", {
+  speed: 75,
+  startDelay: 20500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect9", {
+  speed: 75,
+  startDelay: 22500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect10", {
+  speed: 75,
+  startDelay: 24500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect11", {
+  speed: 75,
+  startDelay: 26500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect12", {
+  speed: 75,
+  startDelay: 28500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect13", {
+  speed: 75,
+  startDelay: 30500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect14", {
+  speed: 30,
+  startDelay: 33500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect15", {
+  speed: 30,
+  startDelay: 34500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".console-effect3", {
+  speed: 5,
+  startDelay: 35500,
+  lifeLike: false,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect16", {
+  speed: 30,
+  startDelay: 37500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect17", {
+  speed: 30,
+  startDelay: 38500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".console-effect4", {
+  speed: 5,
+  startDelay: 40500,
+  lifeLike: false,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect18", {
+  speed: 30,
+  startDelay: 42500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect19", {
+  speed: 30,
+  startDelay: 43500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".console-effect5", {
+  speed: 5,
+  startDelay: 44500,
+  lifeLike: false,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect20", {
+  speed: 30,
+  startDelay: 46500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect21", {
+  speed: 30,
+  startDelay: 47500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".console-effect6", {
+  speed: 5,
+  startDelay: 48500,
+  lifeLike: false,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect22", {
+  speed: 30,
+  startDelay: 50500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect23", {
+  speed: 30,
+  startDelay: 51500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".console-effect7", {
+  speed: 5,
+  startDelay: 52500,
+  lifeLike: false,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect24", {
+  speed: 30,
+  startDelay: 54500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect25", {
+  speed: 30,
+  startDelay: 55500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".console-effect8", {
+  speed: 5,
+  startDelay: 56500,
+  lifeLike: false,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect26", {
+  speed: 30,
+  startDelay: 58500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".type-effect27", {
+  speed: 30,
+  startDelay: 59500,
+  lifeLike: true,
+  cursor: false
+}).go();
+
+new TypeIt(".console-effect9", {
+  speed: 5,
+  startDelay: 60500,
+  lifeLike: false,
+  cursor: false
+}).go();
